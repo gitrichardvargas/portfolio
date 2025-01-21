625ca3f0af005b9d574494ac0f3429b8ee18c80a
Richard Vargas <emailrichardvargas@gmail.com>
2025-01-21 02:30:08 -0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,28 @@
+if (/Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor)) {
+    const sectionContainer = document.querySelector('.section-container');
+
+    function adjustPadding() {
+        if (window.innerWidth < 900) {
+            sectionContainer.style.paddingTop = '1750px';
+        } else {
+            sectionContainer.style.paddingTop = '1500px';
+        }
+    }
+
+    window.addEventListener('resize', adjustPadding);
+    adjustPadding(); // Set initial padding based on current width
+}
+
+
+let currentSlide = 0;
+
+function moveSlide(direction) {
+    const slides = document.querySelector('.carousel-slides');
+    const totalSlides = slides.children.length;
+
+    currentSlide = (currentSlide + direction + totalSlides) % totalSlides;
+    const offset = -currentSlide * 100;
+
+    slides.style.transform = `translateX(${offset}%)`;
+}
+
